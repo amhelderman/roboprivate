d5826b7f9525901197fae40b4b5afdbd9e171dbb
Alex Helderman <Alexhelderman@gmail.com>
2024-02-05 14:45:27 -0600

    Play video (at incorrect speed) using opencv

--- robo_012_opencv/example.cpp
+++ robo_012_opencv/example.cpp
@@ -2,8 +2,23 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <stdexcept>
 
 using namespace cv;
+
+
+VideoCapture setupVideo() {
+      //open the video file for reading
+     VideoCapture cap("/pwd/sample.webm"); 
+
+     // if not success, exit program
+     if (cap.isOpened() == false)  
+     {
+          throw std::invalid_argument("Cannot open the video file");
+     }
+
+     return cap;
+}
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
@@ -26,16 +41,40 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
      }
 }
 
-int main()
-{
+Mat frameWithCircle() {
+
     Mat img(200, 200, CV_8UC3, Scalar(0, 0, 0));
 
     Scalar color = Scalar(255, 0, 0);
     circle(img, Point(30, 30), 10, color, 3);
-
-    imshow("Display window", img);
-    setMouseCallback("Display window", CallBackFunc, NULL);
     
-    int k = waitKey(0); // Wait for a keystroke in the window
-    return 0;
+    return img;
+}
+
+int main()
+{
+
+
+     imshow("Display window", frameWithCircle());
+     setMouseCallback("Display window", CallBackFunc, NULL);
+
+     VideoCapture cap = setupVideo();
+
+     while (true) {
+          Mat frame;
+          
+          if (!cap.read(frame)) {
+               std::cout << "End of video" << std::endl;
+               break;
+          }
+
+          imshow("Display window", frame);
+
+          if (waitKey(10) == 27) {
+               std::cout << "Esc key is pressed by user. Stopping the video" << std::endl;
+               break;
+          }
+          
+     }
+     return 0;
 }
